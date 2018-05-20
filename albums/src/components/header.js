198e5556c3a906a88c323d2f3df8c7f9af4a73c5
Unknown <luke.towell@hotmail.com>
2018-05-20 16:13:32 -0500

    Header component changes and implementation
    - Added in the header component to the index.js so that it now uses that component.
    - Added in the correct header text and added in the relevant styles. to the header.

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,13 +1,32 @@
 //Imprt a library to create a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 
 //Create a component
 const Header = () => {
     return (
-    <Text>This is a header</Text>
+        <View style={styles.viewStyle}>
+            <Text style={styles.textStyle}>Album!</Text>;
+        </View>
     );
+};
+
+const styles = {
+    textStyle: {
+        fontSize: 40,
+        color: 'white'  
+    },
+    viewStyle: {
+        marginTop: 20,
+        height: 80,
+        backgroundColor: '#28303C',
+        justifyContent: 'center',
+        alignItems: 'center',
+        shadowColor: 'white',
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 0.2,        
+    }
 };
 
 //Render it to the device
