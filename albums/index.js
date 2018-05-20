92e19a060e74243a0e8ad8788d1f260381eaf780
Unknown <luke.towell@hotmail.com>
2018-05-20 15:15:03 -0500

    ESLint and changes to the index.js file
    - Added in ESLint 
    - Made changes to the App component in the Index.js file.

--- albums/index.js
+++ albums/index.js
@@ -1,4 +1,12 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+//Imprt a library to create a component
+import React from 'react';
+import { AppRegistry, Text } from 'react-native';
 
+
+//Create a component
+const App = () => (
+    <Text>Some Text</Text>
+);
+
+//Render it to the device
 AppRegistry.registerComponent('albums', () => App);
