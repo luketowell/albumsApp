7c63ef94aba984b3cf40e327de56ab28ab38a1bd
Unknown <luke.towell@hotmail.com>
2018-05-20 15:16:07 -0500

    Added in components
    - Added in a component structure to the application.
    - Added in a header component and did the basic set up of the application.

---
+++ albums/src/components/header.js
@@ -0,0 +1,14 @@
+//Imprt a library to create a component
+import React from 'react';
+import { Text } from 'react-native';
+
+
+//Create a component
+const Header = () => {
+    return (
+    <Text>This is a header</Text>
+    );
+};
+
+//Render it to the device
+export default Header;
