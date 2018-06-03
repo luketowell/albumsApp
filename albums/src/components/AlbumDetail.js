782858ea70de2507ee3f32645a89b254e295284b
Unknown <luke.towell@hotmail.com>
2018-06-03 19:58:05 +0100

    Added in the album detail and did some styling using flex box on the images
    - Did some styling on the images and the artist name and album title.
    - refactored the way that props and styles are being referenced.

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,16 +1,64 @@
-import React, { Component } from 'react';
-import { Text, View, Image, Button } from 'react-native'
+import React from 'react';
+import { View, Text, Image, Button } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
-   return(
-   <View>
-        <Image style={{width:100, height:100}} source={{url: props.albumDetails.thumbnail_image}} />
-        <Text>{props.albumDetails.title}</Text>
-        <Text>By {props.albumDetails.artist}</Text>
-        <Button title='Buy Now' color='blue' accessibilityLabel='Click here to buy the album'/>
-    </View>
+const AlbumDetail = ({ albumDetails }) => {
+    const { title, artist, thumbnail_image, image, url } = albumDetails;
+    const { albumTextStyles, textStyles, textContainer, thumbnailContainer, imageStyle, imageViewStyle } = styles;
+
+   return (
+   <Card>
+       <CardSection>
+           <View>
+                <Image 
+                style={thumbnailContainer}
+                source={{ url: thumbnail_image }} 
+                />
+            </View>
+            <View style={textContainer}>
+                <Text style={albumTextStyles}>{title}</Text>
+                <Text style={textStyles}>{artist}</Text>
+            </View>
+        </CardSection>
+        <CardSection style={imageViewStyle}>
+            <Image 
+            style={imageStyle}
+            source={{ url: image }} 
+            />
+        </CardSection>
+        <CardSection>
+            <Button title='Buy Now' color='blue' accessibilityLabel='Click here to buy the album' />
+        </CardSection>
+    </Card>
    );
+};
 
-}
+const styles = {
+    albumTextStyles: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    textStyles: {
+        fontSize: 12,
+    },
+    textContainer: {
+        justifyContent: 'space-around',
+        flexDirection: 'column',
+    },
+    thumbnailContainer: {
+        width: 50,
+        height: 50,
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1
+    },
+    imageViewStyle: {
+        
+    }
+};
 
 export default AlbumDetail;
